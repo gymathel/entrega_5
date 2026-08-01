42582eac0bde536c6ac81eb61b0c6ca9db4f5170
Gabriel Gonzalez <gonzalezlopez.gabrielalberto@gmail.com>
2026-08-01 11:03:42 -0300

    Pre-Entrega 2: Logica de programacion y control de flujo

--- clase1/js/main.js
+++
@@ -1,7 +0,0 @@
-console.log("Te voy a pedir darm tu Nombre, Apellido y Año de Nacimiento")
-let nombre= prompt("Ingresa tu nombre");
-let apellido= prompt("Ingresa tu apellido");
-let nacimiento= prompt("Ingresa tu año de nacimiento");
-nacimiento =  parseInt(nacimiento);
-
-console.log(nombre + " " + apellido + ", tu edad es: " + (2026-nacimiento));
